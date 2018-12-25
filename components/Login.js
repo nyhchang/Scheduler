0da9e829276c4d1afcce98f835d004c7f9cc6cf4
nyhchang <nyhchang222@gmail.com>
2018-12-24 17:47:12 -0800

    minor changes

---
+++ components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <View>
+          /*Empty for now but look into react native elements docs and find out how to put in some dank login page stuff
+           */
+        </View>
+    );
+  }
+}
